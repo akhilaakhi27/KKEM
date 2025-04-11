21fa1bbf46bbad2a3768ca021048e742f286bf85
akhilaakhi27 <akhilaaravind99@gmail.com>
2025-04-11 13:14:26 +0530

    init

--- js_basics/script.js
+++ js_basics/script.js
@@ -2,11 +2,15 @@
 // let a=12;
 // console.log(a)
 
-let a={
-    name:'Anitha',
-    age:23
-}
-console.log(typeof(a))
+// let a={
+//     name:'Anitha',
+//     age:23
+// }
+// console.log(typeof(a))
 
-let b=['apple','orange','cherry','banana']
-console.log(b[0])
+// let b=['apple','orange','cherry','banana']
+// console.log(b[0])
+
+function btnClick(){
+    alert("Button is clicked");
+}
