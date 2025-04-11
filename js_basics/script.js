23b856228abf02c4aedebd20ef7047713cc8f3b0
akhilaakhi27 <akhilaaravind99@gmail.com>
2025-04-11 14:32:12 +0530

    initial commit

--- js_basics/script.js
+++ js_basics/script.js
@@ -11,6 +11,24 @@
 // let b=['apple','orange','cherry','banana']
 // console.log(b[0])
 
-function btnClick(){
-    alert("Button is clicked");
+// function btnClick(){
+//     alert("Button is clicked");
+// }
+
+// function btnClick(){
+//     prompt("Button is clicked");
+// }
+
+function btnClick()
+{
+let username=document.getElementById('t1').value;
+let password=document.getElementById('p1').value;
+if(username==""||password==""){
+    alert("Enter username and password");
+    return false;
+}
+else{
+    alert("login successful");
+    return true;
+}
 }
