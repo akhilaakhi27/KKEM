345ce47f175dc8f2c5e00bc383bc00a4bf868b51
akhilaakhi27 <akhilaaravind99@gmail.com>
2025-04-11 13:09:51 +0530

    init

--- js_basics/script.js
+++ js_basics/script.js
@@ -1,1 +1,12 @@
-console.log("hello world")
+// console.log("hello world")
+// let a=12;
+// console.log(a)
+
+let a={
+    name:'Anitha',
+    age:23
+}
+console.log(typeof(a))
+
+let b=['apple','orange','cherry','banana']
+console.log(b[0])
